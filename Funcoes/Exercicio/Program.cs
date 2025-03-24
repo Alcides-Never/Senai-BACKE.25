12af801dd6cf1e641514fa6dd14c6cf580c64de1
Alcides Paz <alcidesnp@hotmail.com>
2025-03-24 20:57:05 -0300

    update: aula sobre funções

---
+++ Funcoes/Exercicio/Program.cs
@@ -0,0 +1,74 @@
+﻿// 1
+
+using System.Threading.Channels;
+
+string Saudacao(string nome)
+{
+    string mensagem = "Olá " + nome + "! Bem vindo!";
+    
+    return mensagem;
+}
+Console.WriteLine(Saudacao("Carlos"));
+
+// 2
+
+int Soma (int a, int b)
+{
+    return a + b;
+}
+int resultado = Soma(5, 3);
+Console.WriteLine(resultado);
+
+// 3 
+
+void MostrarMensagem ()
+{
+    Console.WriteLine("Este é um programa de C# com funções!");
+}
+
+MostrarMensagem();
+
+// 4
+
+double CalcularMedia(double a, double b, double c)
+{
+    return (a + b + c) / 3;
+}
+
+double media = CalcularMedia(7.5, 8.0, 6.5);
+Console.WriteLine("Media: " + media);
+
+// 5
+
+bool EhPar(int num)
+{
+    if(num % 2 == 0)
+    {
+        return true;
+    } else
+    {
+        return false;
+    }
+}
+
+bool resultadoPar = EhPar(10);
+Console.WriteLine(resultadoPar);
+
+// 6 
+
+double CelsiusParaFahrenheit (int C)
+{
+    double F = (C * 9 / 5) + 32;
+    return F;
+}
+
+double fahrenheit = CelsiusParaFahrenheit(25);
+Console.WriteLine(fahrenheit);
+
+// 7 
+
+int ContarVogais (string palavra)
+{
+    string teste[] = palavra.Split(' ');
+    return teste;
+}
---
+++ Funcoes/Funcoes/Program.cs
@@ -0,0 +1,30 @@
+﻿// Funcoes
+// Ações - Passos - Funcionalidades
+
+// Mostre uma mensagem de Boa noite
+// Função com retorno
+
+// parametro
+string MostrarMensagem (string nome)
+{
+    string mensagem = "Boa noite!" + nome;
+
+    return mensagem;
+}
+
+string nomeUsuario = Console.ReadLine();
+
+Console.WriteLine(MostrarMensagem(nomeUsuario));
+
+string mensagemBoaNoite = MostrarMensagem("Vinicio");
+Console.WriteLine(mensagemBoaNoite + "Seja bem vindo!");
+
+// Função sem Retorno
+// Função que exibe a idade
+
+void ExibirIdade(int idade)
+{
+    Console.WriteLine("A idade da pessoa é " + idade);
+}
+
+ExibirIdade(26);
