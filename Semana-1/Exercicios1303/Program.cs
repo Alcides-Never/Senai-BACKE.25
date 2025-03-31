3e68bce1b214231e4730b78872a1953870b1193d
Alcides Paz <alcidesnp@hotmail.com>
2025-03-31 12:55:40 -0300

    update: Continuação dos exercicios de condicionais

--- Semana-1/Exercicios1303/Program.cs
+++ Semana-1/Exercicios1303/Program.cs
@@ -59,9 +59,51 @@ if (numeroMaior1 > numeroMaior2 && numeroMaior1 > numeroMaior3)
 }
 
 //5 
+Console.WriteLine("Bhaskara");
 int a = int.Parse(Console.ReadLine());
 int b = int.Parse(Console.ReadLine());
 int c = int.Parse(Console.ReadLine());
 
 double delta = ((Math.Pow(b, 2)) - (4 * a * c));
 
+double x1 = ((-b) + (delta)) / (2 * a);
+double x2 = ((-b) - (delta)) / (2 * a);
+Console.WriteLine($"Valor de delta {delta}");
+Console.WriteLine($"Valor de x1 = {x1}");
+Console.WriteLine($"Valor de x2 = {x2}");
+
+//6
+Console.WriteLine("Calculadora");
+Console.WriteLine("Digite o primeiro valor: ");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite o operador de cálculo: +, -, /, ou *");
+char operador = char.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo valor: ");
+double num2 = double.Parse(Console.ReadLine());
+
+if (operador == '+')
+{
+    Console.WriteLine("Resultado: " + (num1 + num2));
+} else if (operador == '-')
+{
+    Console.WriteLine("Resultado: " + (num1 - num2));
+} else if (operador == '*')
+{
+    Console.WriteLine("Resultado: " + (num1 * num2));
+} else if (operador == '/')
+{
+    if (num1 != 0)
+    {
+        Console.WriteLine("Resultado: " + (num1 / num2));
+    }
+    else
+    {
+        Console.WriteLine("Erro: Divisão por zero");
+    }
+}
+else
+{
+    Console.WriteLine("Operador Inválido");
+}
+
+// 7 
